7647b228ac8925b9d7a8e117f76cb7cae5f5bb09
david.fonseca <fonsecasoundcruz@gmail.com>
2023-01-20 00:51:25 -0600

    Capitulo 70 creación de submenus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,8 +33,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PopupsModule } from './shared/popups';
 
 import { NotificationModule } from './services';
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+
+import {  MatToolbarModule} from "@angular/material/toolbar";
+
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { HeaderComponent } from './components/header/header.component';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MenuListComponent } from './components/menu-list/menu-list.component';
+
+import { MatListModule } from "@angular/material/list";
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, MenuListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -51,7 +65,13 @@ import { NotificationModule } from './services';
     IndicatorsModule,
     BrowserAnimationsModule,
     PopupsModule,
-    NotificationModule.forRoot()
+    NotificationModule.forRoot(),
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent],
