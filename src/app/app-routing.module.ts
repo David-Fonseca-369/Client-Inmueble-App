0280046c36cc466d2d5dbf5a578606cd9bf44048
david.fonseca <fonsecasoundcruz@gmail.com>
2023-01-21 00:31:27 -0600

    Cap. 73 Crear páginas estáticas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    //hijos
+    children: [
+      {
+        path: 'static',
+        loadChildren: () =>
+          import('./pages/static/static.module').then((m) => m.StaticModule),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        //como ya se ha referenciado arriba, ya se pueden acceder a las páginas de static.
+        redirectTo: 'static/welcome',
+      },
+    ],
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'static/404',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
