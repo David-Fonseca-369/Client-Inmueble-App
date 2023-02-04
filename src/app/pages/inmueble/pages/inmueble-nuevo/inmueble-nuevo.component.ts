7c18d83363be13a5f5247685314bb697b664c632
david.fonseca <fonsecasoundcruz@gmail.com>
2023-02-04 00:29:18 -0600

    Addcap 99

--- src/app/pages/inmueble/pages/inmueble-nuevo/inmueble-nuevo.component.ts
+++ src/app/pages/inmueble/pages/inmueble-nuevo/inmueble-nuevo.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-inmueble-nuevo',
   templateUrl: './inmueble-nuevo.component.html',
   styleUrls: ['./inmueble-nuevo.component.scss']
 })
-export class InmuebleNuevoComponent {
+
+export class InmuebleNuevoComponent implements OnInit{
+
+ loading$!: Observable<boolean|null>;
+ photoLoaded!: string;
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+  }
+
+
+  registrarInmueble(form: NgForm): void{
+
+  }
+
+
+  onFilesChanged(url: any): void{
+    if (url) {
+      this.photoLoaded  = url;
+    }
+  }
+
+
+
+
 
 }
